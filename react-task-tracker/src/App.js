896b697e0eaa5cf3e40f0be1e68ddd8e8cef56e0
shubham-pal-au9 <spal5776@gmail.com>
2021-03-02 16:18:25 +0530

    Adding_Hooks

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,34 @@
 import Header from './components/Header';
-function App() {
-  
+import Tasks from './components/Tasks';
+import{ useState } from 'react';
+const App = () => {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id:1,
+            text:'Doctor Appointment',
+            day:'Mar 2nd at 3:41pm',
+            reminder:true
+        },
+        {
+            id:2,
+            text:'Meeting at the college',
+            day:'Jan 1st at 2:20pm',
+            reminder:true
+        },
+        {
+            id:3,
+            text:'Clothes shopping',
+            day:'Feb 5th at 1:30am',
+            reminder:true
+        }
+    ]
+)
+
   return (   
     <div className="container"> 
       <Header/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
