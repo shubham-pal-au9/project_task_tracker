f35234b337c8a8a4f1d66c48d57c6618e2ec985c
shubham-pal-au9 <spal5776@gmail.com>
2021-03-04 23:29:02 +0530

    Updated_header

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -42,14 +42,14 @@ const deleteTask = (id) => {
   // Toggle Reminder
   const toggleReminder = (id) => {
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task
-    
     ))
   }
 
 
   return (   
     <div className="container"> 
-      <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask}/>
       {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> :'No tasks to display'}
     </div>
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
-const Header = ({title,onAdd}) => {
+const Header = ({title,onAdd, showAdd}) => {
     
     return (
         <header className = 'header'>
             <h1>{title}</h1>
-            <Button color='blue' text='Add' onClick={onAdd} />
+            <Button 
+            color={showAdd ? 'blue' : 'red'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}
+            />
         </header>
     )
 } 
